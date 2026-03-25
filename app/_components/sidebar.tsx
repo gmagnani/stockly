0b501f7422e4b777cc6bb8df90fa3890ac83305a
gmagnani <magnanigeovanni@gmail.com>
2026-03-25 11:55:03 -0300

    chore: add tailwind prettier plugin

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,13 +1,13 @@
 const Sidebar = () => {
   return (
-    <div className="w-64 bg-zinc-50 ">
+    <div className="w-64 bg-zinc-50">
       <div className="px-8 py-6">
         <h1 className="text-2xl font-bold">Stockly</h1>
       </div>
       <div className="flex flex-col gap-2 p-2">
-        <button className="px-6 py-2 rounded-md bg-zinc-100">Dashboard</button>
-        <button className="px-6 py-2 rounded-md bg-zinc-100">Produtos</button>
-        <button className="px-6 py-2 rounded-md bg-zinc-100">Pedidos</button>
+        <button className="rounded-md bg-zinc-100 px-6 py-2">Dashboard</button>
+        <button className="rounded-md bg-zinc-100 px-6 py-2">Produtos</button>
+        <button className="rounded-md bg-zinc-100 px-6 py-2">Pedidos</button>
       </div>
     </div>
   );
