a58895b38557ec092b2c81ead6c72bb4b8b9c824
gmagnani <magnanigeovanni@gmail.com>
2026-03-26 11:58:08 -0300

    refactor: add data access layer

---
+++ app/_data-access/product/get-products.ts
@@ -0,0 +1,12 @@
+import { db } from "@/lib/prisma";
+import { Product } from "@/generated/prisma/client";
+
+export const getProducts = async (): Promise<Product[]> => {
+  try {
+    const products = await db.product.findMany();
+    return products;
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return [];
+  }
+};
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,12 +1,11 @@
-import { db } from "@/lib/prisma";
-
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Plus } from "lucide-react";
 import { columns } from "./_components/table-columns";
+import { getProducts } from "@/app/_data-access/product/get-products";
 
 const ProductsPage = async () => {
-  const products = await db.product.findMany();
+  const products = await getProducts();
   return (
     <div className="m-8 w-full space-y-8 rounded-lg bg-white p-6">
       <div className="flex w-full items-center justify-between">
