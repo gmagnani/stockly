2cc50bf91aaa70044c7e91cc43789257388c2c35
gmagnani <magnanigeovanni@gmail.com>
2026-03-30 13:22:42 -0300

    feat: add dropdown menu

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,8 +1,11 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { Product } from "@/generated/prisma/browser";
 import { ColumnDef } from "@tanstack/react-table";
+import { ClipboardCopyIcon, EditIcon, MoreHorizontal, TrashIcon } from "lucide-react";
 
 const getStatus = (status: string) => {
   if (status === "IN_STOCK") return "Em Estoque";
@@ -29,5 +32,39 @@ export const columns: ColumnDef<Product>[] = [
       const product = row.original;
       return <Badge variant={product.status === "IN_STOCK" ? "default" : "destructive"}>{getStatus(product.status)}</Badge>;
     },
-  },
+  },{
+    id: "actions",
+    header: "Ações",
+    cell: ({ row }) => {
+      const product = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Abrir menu</span>
+              <MoreHorizontal size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator /> 
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(product.id)}
+            >
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <EditIcon size={16} />
+              Editar
+              </DropdownMenuItem>
+            <DropdownMenuItem>
+              <TrashIcon size={16} />
+              Excluir
+              </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  }
 ];
